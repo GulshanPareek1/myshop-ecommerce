c9df8293353345fc1a3bc6f43e8a5dcdc73dbc7c
GulshanPareek1 <pareekgippy5@gmail.com>
2024-04-01 01:06:31 +0530

    checkout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,9 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import { Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 //write some code for this component?
 function App() {
 	return (
@@ -14,6 +17,7 @@ function App() {
 				</Container>
 			</main>
 			<Footer />
+			<ToastContainer />
 		</>
 	);
 }
