c7c79a0026ff236f41b072c5837391700dae8c8e
GulshanPareek1 <pareekgippy5@gmail.com>
2024-04-02 20:56:24 +0530

    Product  feature By Admin

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -4,10 +4,18 @@ const products = require("../data/product");
 const {
 	getProducts,
 	getProductById,
+	createProduct,
+	updateProduct,
+	deleteProduct,
 } = require("../controllers/productController");
+const { protect, admin } = require("../middleware/authMiddleware");
 
-router.route("/").get(getProducts);
-router.route("/:id").get(getProductById);
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router
+	.route("/:id")
+	.get(getProductById)
+	.put(protect, admin, updateProduct)
+	.delete(protect, admin, deleteProduct);
 
 // router.get("/:id", getProductById);
 
